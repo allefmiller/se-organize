de8b111cdea49a3c0acb41e167c9191e26caaef5
Allef Miller <allefmiller@hotmail.com>
2023-01-04 21:11:39 -0300

    iniciando projeto spring

---
+++ src/main/java/com/api/seorganize/SeOrganizeApplication.java
@@ -0,0 +1,13 @@
+package com.api.seorganize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeOrganizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeOrganizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/seorganize/SeOrganizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.seorganize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeOrganizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
